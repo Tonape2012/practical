ff6b1ba7be0a6ee7eb1a9bba7bc7e6704124f061
Tonape2012 <127720871+Tonape2012@users.noreply.github.com>
2023-03-15 19:31:15 +0530

    Add files via upload

---
+++ Area_Perimeter.java
@@ -0,0 +1,11 @@
+class Area_Perimeter{
+public static void main(String args[]){
+System.out.println("Area of rectangle is:");
+System.out.println("perimeter of rectangle is:");
+int length = 8;
+int breadth = 6;
+System.out.println(length*breadth);
+System.out.println(2*length+breadth);
+
+}
+}
