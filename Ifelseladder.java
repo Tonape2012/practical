f7dc5ccf68d3a8e8757bbf7196b9fa01d183d1c6
Tonape2012 <127720871+Tonape2012@users.noreply.github.com>
2023-03-17 13:46:52 +0530

    Add files via upload

---
+++ Ifelseladder.java
@@ -0,0 +1,17 @@
+class Ifelseladder{
+public static void main(String args[]){
+String name ="Aishwarya"; 
+if(name == "Riya"){
+System.out.println("name is riya");
+}
+else if(name == "Neha"){
+System.out.println("name is neha");
+}
+else if(name == "Tina"){
+System.out.println("name is tina");
+}
+else{
+System.out.println(name);
+}
+}
+}
