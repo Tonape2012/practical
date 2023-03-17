12eb89ec4b2802322eb2a3a4da79a47d1001dd17
Tonape2012 <127720871+Tonape2012@users.noreply.github.com>
2023-03-17 12:11:14 +0530

    Add files via upload

---
+++ If_else.java
@@ -0,0 +1,12 @@
+class If_else{
+public static void main(String args[]){
+int x = 3;
+int y = 5;
+if(x+y < 7){
+System.out.println("x+y is less than 10");
+}
+else{
+System.out.println("x+y is greater than 10");
+}
+}
+}
---
+++ Simpleif.java
@@ -0,0 +1,9 @@
+class Simpleif{
+public static void main(String args[]){
+int x = 3;
+int y = 5;
+if(x+y < 10){
+System.out.println("x+y is less than 10");
+}
+}
+}
